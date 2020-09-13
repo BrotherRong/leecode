d9007785cbd2419cacc3e4d72f55d89dc056e076
荣翔 <297395782@qq.com>
2020-09-13 23:25:27 +0800

    no message

---
+++ Python/17_letterCombinations.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2020/9/13 23:21
+# @Author   : Rong
+# @email    : mrrong0720@163.com
+
+# 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+#
+# 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+#
+# 示例:
+# 输入："23"
+# 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+
+        def backtrack(conbination, nextdigit):
+            if len(nextdigit) == 0:
+                res.append(conbination)
+            else:
+                for letter in phone[nextdigit[0]]:
+                    backtrack(conbination + letter, nextdigit[1:])
+
+        res = []
+        backtrack('', digits)
+        return res
+
+    def letterCombinations1(self, digits: str) -> List[str]:
+        if not digits: return []
+        phone = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+        queue = ['']  # 初始化队列
+        for digit in digits:
+            for _ in range(len(queue)):
+                tmp = queue.pop(0)
+                for letter in phone[ord(digit) - 50]:  # 这里我们不使用 int() 转换字符串，使用ASCII码
+                    queue.append(tmp + letter)
+        return queue
+
+s = Solution()
+print(s.letterCombinations1("23"))
