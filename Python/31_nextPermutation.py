fcfcb2671af93bb3faf2f2984298ecebafa345e4
荣翔 <297395782@qq.com>
2020-09-27 18:53:46 +0800

    no message

--- Python/31_nextPermutation.py
+++ Python/31_nextPermutation.py
@@ -4,6 +4,14 @@
 # @email    : mrrong0720@163.com
 from typing import List
 
+# 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+# 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+# 必须原地修改，只允许使用额外常数空间。
+# 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+# 1,2,3 → 1,3,2
+# 3,2,1 → 1,2,3
+# 1,1,5 → 1,5,1
+
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
@@ -25,4 +33,4 @@ class Solution:
 
 
 s = Solution()
-print(s.nextPermutation([1,2,3,4,5]))
+print(s.nextPermutation([1,2,3]))
